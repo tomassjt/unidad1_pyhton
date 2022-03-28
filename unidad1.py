121511a56412f0ab74c8a0c06cf5b8a59d9c12b6
tomassjt <tomasjtome@gmail.com>
2022-03-27 22:04:37 -0300

    Second commit

--- unidad1.py
+++ unidad1.py
@@ -1,2 +1,13 @@
-saludo ="hola"
-print(saludo)
+#we perform basic operations with python unit one bioengineering
+#Relational Operators
+
+a = 12
+b = 5
+
+print('a<b: ', a<b)
+print('a>b: ', a>b)
+print('a==b:', a==b)
+print('a>=b:', a>=b)
+print('a<=b:', a<=b)
+print('a!=b:', a!=b)
+ 
