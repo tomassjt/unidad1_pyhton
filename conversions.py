3949ed614292c288e85a964f2fa1e30080626f6e
tomassjt <tomasjtome@gmail.com>
2022-03-28 09:08:28 -0300

    Resolver input en visual studio code

---
+++ conversions.py
@@ -0,0 +1,22 @@
+a = 5            # a es de tipo int y su valor es 5
+print(a)
+b = float( a )   # b es de tipo float y su valor es 5.0 
+print(b)
+x = 10.9         # x es de tipo float y su valor es 10.9
+print(x)
+y = int( x )     # y es de tipo int y su valor es 10 
+print(y)
+m = 25           # m es de tipo int y su valor es 25
+print(m)
+p = str( m )     # p es de tipo str y su valor es ‘25’
+print(p)
+
+
+#To know the type of data assigned to a variable we can use the type() function
+tipo = type(p)
+print('the type of P:',tipo)
+ 
+ 
+variable2 = input('Ingrese valor: ')
+print('la variable ingresada es:',variable2)
+print(variable2)
